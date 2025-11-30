962233860a01f25d49931904d396fd3b9e6f1d66
Naveen Anpalagan <navu9@hotmail.com>
2025-11-30 23:47:54 +0100

    day 1

--- days/day-script.js
+++ days/day-script.js
@@ -1,4 +1,14 @@
 function typeMessage() {
+    const audio = document.getElementById('background-music');
+        if (audio) {
+            audio.muted = true; // Set muted property to false
+            audio.pause();        // Ensure it starts playing right away
+        }
+    const music = document.getElementById('music');
+        if (music) {
+            music.muted = false; // Set muted property to false
+            music.play();        // Ensure it starts playing right away
+        }
     const messageElement = document.getElementById('message');
     const fullText = messageElement.textContent.trim();
     messageElement.textContent = ''; // Clear text initially
